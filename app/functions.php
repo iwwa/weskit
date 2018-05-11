0343cc2921f54a5f391ff2f40a1d6c0cebcb9e89
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2018-05-11 10:44:38 -0300

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -17,7 +17,12 @@ function theme_dir() {
 
 
 // Desabilitando a barra adminstrativa
-show_admin_bar( false );
+add_action('after_setup_theme', 'disable_admin_bar');
+function disable_admin_bar() {
+    if ( !current_user_can('administrator') && !is_admin() ) {
+        show_admin_bar(false);
+    }
+}
 
 
 // Habilitando suporte a thumbnails
